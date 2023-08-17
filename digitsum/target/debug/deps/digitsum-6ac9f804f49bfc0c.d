a26249014d84d33e15b78536af554c8e46a6c6f7
winnyace <winnyace82@gmail.com>
2023-08-17 14:11:20 +0300

    I don't even know, to be honest.

---
+++ digitsum/target/debug/deps/digitsum-6ac9f804f49bfc0c.d
@@ -0,0 +1,5 @@
+/home/ion/Documents/Projects/Rust_dump/digitsum/target/debug/deps/digitsum-6ac9f804f49bfc0c: src/main.rs
+
+/home/ion/Documents/Projects/Rust_dump/digitsum/target/debug/deps/digitsum-6ac9f804f49bfc0c.d: src/main.rs
+
+src/main.rs:
---
+++ digitsum/target/debug/deps/text_io-33697e45fde53b32.d
@@ -0,0 +1,7 @@
+/home/ion/Documents/Projects/Rust_dump/digitsum/target/debug/deps/text_io-33697e45fde53b32.rmeta: /home/ion/.cargo/registry/src/index.crates.io-6f17d22bba15001f/text_io-0.1.12/src/lib.rs
+
+/home/ion/Documents/Projects/Rust_dump/digitsum/target/debug/deps/libtext_io-33697e45fde53b32.rlib: /home/ion/.cargo/registry/src/index.crates.io-6f17d22bba15001f/text_io-0.1.12/src/lib.rs
+
+/home/ion/Documents/Projects/Rust_dump/digitsum/target/debug/deps/text_io-33697e45fde53b32.d: /home/ion/.cargo/registry/src/index.crates.io-6f17d22bba15001f/text_io-0.1.12/src/lib.rs
+
+/home/ion/.cargo/registry/src/index.crates.io-6f17d22bba15001f/text_io-0.1.12/src/lib.rs:
